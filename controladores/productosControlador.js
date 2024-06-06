3b04f46f6723bc74a2df8fc4addce9a7501cd9b8
diegohpezet <diegohpezet@gmail.com>
2024-06-06 19:23:29 -0300

    separo controladores de rutas

---
+++ controladores/productosControlador.js
@@ -0,0 +1,70 @@
+const promiseQuery = require('../config/db')
+
+// Controlador de productos
+
+const obtenerTodos = async (req, res) => {
+  // Obtiene todos los usuarios de la base de datos
+  try {
+    const query = "SELECT * FROM productos";
+
+    const productos =  await promiseQuery(query)
+    res.json(productos)
+  } catch (error) {
+    throw err
+  }
+}
+
+const obtener = async (req, res) => {
+  try {
+    const id = req.params.id
+    const query = "SELECT * FROM productos WHERE id = ?"
+  
+    const producto = await promiseQuery(query, [id])
+    res.json(producto) 
+  } catch (error) {
+    throw err
+  }
+}
+
+const crear = async (req, res) => {
+  try {
+    const { nombre, precio, stock } = req.body
+    const query = "INSERT INTO productos (nombre, precio, stock) VALUES (?, ?, ?)"
+
+    await promiseQuery(query, [nombre, precio, stock])
+    res.json({message: "Producto creado!!!"})
+  } catch (error) {
+    throw error
+  }
+}
+
+const actualizar = async (req, res) => {
+  try {
+    const {nombre, precio, stock} = req.body
+    const query = "UPDATE productos SET nombre = ?, precio = ?, stock = ? WHERE id = ?"
+    
+    await promiseQuery(query, [nombre, precio, stock, req.params.id])
+    res.json({message: "Producto actualizado exitosamente"})
+  } catch (error) {
+    throw error
+  }
+}
+
+const borrar = async (req, res) => {
+  try {
+    const query = "DELETE FROM productos WHERE id = ?"
+
+    await promiseQuery(query, [req.params.id])
+    res.json({message: "Producto borrado"})
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  obtenerTodos,
+  obtener,
+  crear,
+  actualizar,
+  borrar
+}
--- rutas/productos.js
+++ rutas/productos.js
@@ -1,44 +1,16 @@
 const express = require('express');
 const router = express.Router()
-const promiseQuery = require('../config/db')
+
+// Importamos controlador
+const productosController = require('../controladores/productosControlador')
 
 // /productos
 
 // localhost:3000/productos/
-router.get('/', async (req, res) => {
-  try {
-    const query = "SELECT * FROM productos";
-
-    const productos =  await promiseQuery(query)
-    res.json(productos)
-  } catch (error) {
-    throw err
-  }
-})
-
-router.get("/:id", async (req, res) => {
-  try {
-    const id = req.params.id
-    const query = "SELECT * FROM productos WHERE id = ?"
-  
-    const producto = await promiseQuery(query, [id])
-    res.json(producto) 
-  } catch (error) {
-    throw err
-  }
-})
-
-router.post('/', async (req, res) => {
-  try {
-    const { nombre, precio, stock } = req.body
-    const query = "INSERT INTO productos (nombre, precio, stock) VALUES (?, ?, ?)"
-
-    await promiseQuery(query, [nombre, precio, stock])
-    res.json({message: "Producto creado!!!"})
-  } catch (error) {
-    throw error
-  }
-})
-
+router.get('/', productosController.obtenerTodos);
+router.get("/:id", productosController.obtener);
+router.post('/', productosController.crear);
+router.put('/:id', productosController.actualizar);
+router.delete('/:id', productosController.borrar);
 
 module.exports = router
